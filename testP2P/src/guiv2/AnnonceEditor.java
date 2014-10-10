1df72eca56488c787f5224080584ab4fb11662d4
Prudhomme Julien <julien.prudhomme@etu.univ-amu.fr>
2014-10-10 17:53:21 +0200

    La partie edition d'annonce à été améliorée (presque finie) Ajout d'une interface pour la gestion des utilisateurs

--- testP2P/src/guiv2/AnnonceEditor.java
+++ testP2P/src/guiv2/AnnonceEditor.java
@@ -5,6 +5,7 @@ import java.awt.FlowLayout;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JFileChooser;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
@@ -36,18 +37,23 @@ import net.atlanticbb.tantlinger.shef.HTMLEditorPane;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Dimension;
+import javax.swing.ImageIcon;
 
 public class AnnonceEditor extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
 	private JTextField textField;
 	private JTextField textField_1;
+	private JLabel imageLabel;
 
 	/**
 	 * Create the dialog.
 	 */
 	public AnnonceEditor() {
-		setBounds(100, 100, 450, 462);
+		setModal(true);
+		setResizable(false);
+		setBounds(100, 100, 659, 714);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
@@ -67,6 +73,7 @@ public class AnnonceEditor extends JDialog {
 		JLabel lblTermesDeLchange = new JLabel(Messages.getString("AnnonceEditor.lblTermesDeLchange.text")); //$NON-NLS-1$
 		
 		JCheckBox chckbxTroc = new JCheckBox(Messages.getString("AnnonceEditor.chckbxTroc.text")); //$NON-NLS-1$
+		chckbxTroc.setSelected(true);
 		
 		JCheckBox chckbxArgent = new JCheckBox(Messages.getString("AnnonceEditor.chckbxArgent.text")); //$NON-NLS-1$
 		
@@ -94,19 +101,34 @@ public class AnnonceEditor extends JDialog {
 		
 		JSeparator separator_2 = new JSeparator();
 		
-		JLabel lblDescription = new JLabel(Messages.getString("AnnonceEditor.lblDescription.text_1")); //$NON-NLS-1$
+		JLabel lblDescription = new JLabel(Messages.getString("completeDesc")); //$NON-NLS-1$
 		
 		JPanel editorPane = new HTMLEditorPane();
+		
+		JLabel lblImageAperu = new JLabel(Messages.getString("AnnonceEditor.lblImageAperu.text_1"));
+		
+		JButton btnNewButton = new JButton(Messages.getString("AnnonceEditor.btnNewButton.text")); //$NON-NLS-1$
+		imageLabel = new JLabel(Messages.getString("AnnonceEditor.label.text")); //$NON-NLS-1$
+		imageLabel.setMinimumSize(new Dimension(100, 100));
+		imageLabel.setMaximumSize(new Dimension(100, 100));
+		imageLabel.setIcon(null);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				ImageDialog dialog = new ImageDialog();
+				dialog.showOpenDialog(null);
+				imageLabel.setIcon(new ImageIcon(dialog.getSelectedFile().toString()));
+			}
+		});
 		GroupLayout gl_contentPanel = new GroupLayout(contentPanel);
 		gl_contentPanel.setHorizontalGroup(
 			gl_contentPanel.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_contentPanel.createSequentialGroup()
-					.addGroup(gl_contentPanel.createParallelGroup(Alignment.TRAILING)
-						.addComponent(editorPane, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 427, Short.MAX_VALUE)
-						.addComponent(separator_2, GroupLayout.DEFAULT_SIZE, 585, Short.MAX_VALUE)
-						.addComponent(separator_1, GroupLayout.DEFAULT_SIZE, 585, Short.MAX_VALUE)
-						.addComponent(lblNewLabel, Alignment.LEADING)
-						.addGroup(Alignment.LEADING, gl_contentPanel.createSequentialGroup()
+					.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
+						.addComponent(editorPane, GroupLayout.PREFERRED_SIZE, 631, Short.MAX_VALUE)
+						.addComponent(separator_2, GroupLayout.DEFAULT_SIZE, 631, Short.MAX_VALUE)
+						.addComponent(separator_1, GroupLayout.DEFAULT_SIZE, 631, Short.MAX_VALUE)
+						.addComponent(lblNewLabel)
+						.addGroup(gl_contentPanel.createSequentialGroup()
 							.addContainerGap()
 							.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
 								.addComponent(lblTypeDeLannonce)
@@ -122,22 +144,29 @@ public class AnnonceEditor extends JDialog {
 									.addGap(18)
 									.addComponent(chckbxArgent)))
 							.addGap(67))
-						.addComponent(separator, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 585, Short.MAX_VALUE)
-						.addComponent(lblDiffusion, Alignment.LEADING)
-						.addGroup(Alignment.LEADING, gl_contentPanel.createSequentialGroup()
+						.addComponent(separator, GroupLayout.DEFAULT_SIZE, 631, Short.MAX_VALUE)
+						.addComponent(lblDiffusion)
+						.addGroup(gl_contentPanel.createSequentialGroup()
 							.addContainerGap()
 							.addComponent(lblRsumDeDescription)
 							.addGap(18)
 							.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING, false)
 								.addComponent(textField_1)
 								.addComponent(textField, GroupLayout.DEFAULT_SIZE, 252, Short.MAX_VALUE)))
-						.addGroup(Alignment.LEADING, gl_contentPanel.createSequentialGroup()
+						.addGroup(gl_contentPanel.createSequentialGroup()
 							.addContainerGap()
 							.addComponent(lblTitre))
-						.addComponent(lblAnnonce, Alignment.LEADING)
-						.addGroup(Alignment.LEADING, gl_contentPanel.createSequentialGroup()
+						.addComponent(lblAnnonce)
+						.addGroup(gl_contentPanel.createSequentialGroup()
 							.addContainerGap()
-							.addComponent(lblDescription)))
+							.addComponent(lblDescription))
+						.addGroup(gl_contentPanel.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(lblImageAperu)
+							.addGap(162)
+							.addComponent(imageLabel)
+							.addPreferredGap(ComponentPlacement.RELATED, 237, Short.MAX_VALUE)
+							.addComponent(btnNewButton)))
 					.addContainerGap())
 		);
 		gl_contentPanel.setVerticalGroup(
@@ -175,7 +204,13 @@ public class AnnonceEditor extends JDialog {
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(lblDescription)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(editorPane, GroupLayout.DEFAULT_SIZE, 127, Short.MAX_VALUE))
+					.addComponent(editorPane, GroupLayout.PREFERRED_SIZE, 251, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_contentPanel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblImageAperu)
+						.addComponent(btnNewButton)
+						.addComponent(imageLabel))
+					.addContainerGap(41, Short.MAX_VALUE))
 		);
 		contentPanel.setLayout(gl_contentPanel);
 		{
@@ -190,6 +225,11 @@ public class AnnonceEditor extends JDialog {
 			}
 			{
 				JButton cancelButton = new JButton(Messages.getString("annuler")); //$NON-NLS-1$
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dispose();
+					}
+				});
 				cancelButton.setActionCommand("Cancel");
 				buttonPane.add(cancelButton);
 			}
