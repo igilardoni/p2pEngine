2984b4594ca9c58d724fbae3bcbefdece8f221b7
Ismael <Ismael@Ismael-PC.home>
2014-10-19 22:07:22 +0200

    Fin des modifications de textes et traductions

--- testP2P/src/view/Window.java
+++ testP2P/src/view/Window.java
@@ -48,10 +48,10 @@ public class Window extends JFrame {
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		
-		JMenu menuOption = new JMenu(Messages.getString("options")); //$NON-NLS-1$
+		JMenu menuOption = new JMenu(Messages.getString("Window.menuOptions.text")); //$NON-NLS-1$
 		menuBar.add(menuOption);
 		
-		JMenu menuAide = new JMenu(Messages.getString("aide")); //$NON-NLS-1$
+		JMenu menuAide = new JMenu(Messages.getString("Window.menuAide.text")); //$NON-NLS-1$
 		menuBar.add(menuAide);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -64,7 +64,7 @@ public class Window extends JFrame {
 		JPanel toolBar = new JPanel();
 		contentPane.add(toolBar, BorderLayout.NORTH);
 		
-		JButton btnNewButton = new JButton(Messages.getString("ajouterAnnonce")); //$NON-NLS-1$
+		JButton btnNewButton = new JButton(Messages.getString("Window.btnAjouterAnnonce")); //$NON-NLS-1$
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				new AnnonceEditor().setVisible(true);
@@ -87,7 +87,7 @@ public class Window extends JFrame {
 			}
 		});
 		
-		compteLabel = new JLabel(Messages.getString("Window.lblVousNtesPas.text")); //$NON-NLS-1$
+		compteLabel = new JLabel(Messages.getString("Window.lblPasConnecte.text")); //$NON-NLS-1$
 		GroupLayout gl_toolBar = new GroupLayout(toolBar);
 		gl_toolBar.setHorizontalGroup(
 			gl_toolBar.createParallelGroup(Alignment.LEADING)
@@ -118,12 +118,12 @@ public class Window extends JFrame {
 	private void show_account_buttons() {
 		User user = Application.getInstance().getUsers().getConnectedUser();
 		if(user != null) {
-			compteLabel.setText("Vous êtes connectés en tant que " + user.getLogin());
-			monCompte.setText("Mon compte");
+			compteLabel.setText(Messages.getString("Window.txtConnecteComme.text") + user.getLogin());
+			monCompte.setText(Messages.getString("Window.txtMonCompte.text"));
 		}
 		else {
-			compteLabel.setText("Vous n'êtes pas connecté");
-			monCompte.setText("Se connecter");
+			compteLabel.setText(Messages.getString("Window.txtPasConnecte.text"));
+			monCompte.setText(Messages.getString("Window.txtSeConnecter.text"));
 		}
 	}
 	
