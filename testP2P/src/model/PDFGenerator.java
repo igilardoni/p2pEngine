f559f61be93d8c1cd7b2221773c56f6a02155466
Ismael <Ismael@Ismael-PC.home>
2014-10-28 21:16:09 +0100

    un oublie sur exception

--- testP2P/src/model/PDFGenerator.java
+++ testP2P/src/model/PDFGenerator.java
@@ -1,11 +1,5 @@
 package model;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Font;
-
 
 
 public class PDFGenerator extends AbstractPdfGenerator {
@@ -18,27 +12,15 @@ public class PDFGenerator extends AbstractPdfGenerator {
 		this.image = model.getImageMap();
 		this.bool = model.getBoolMap();
 		
-		try {createPDF();} 
-		catch (DocumentException e) {e.printStackTrace();} 
-		catch (IOException e) {e.printStackTrace();}
+		createPDF();
 		
 	}
 	
 	protected void addContent(){
 		
-		try { addTexte();}
-		catch (IOException e) {	e.printStackTrace();}
-		catch (DocumentException e) { e.printStackTrace();}
-		
-		
-		try {addImage();}
-		catch (MalformedURLException e) {e.printStackTrace();} 
-		catch (IOException e) {e.printStackTrace();} 
-		catch (DocumentException e) {e.printStackTrace();}
-		
-		try {addCheckBox();} 
-		catch (IOException e) {e.printStackTrace();} 
-		catch (DocumentException e) {e.printStackTrace();}
+		addTexte();
+		addImage();
+		addCheckBox();
 		
 	}	
 }
