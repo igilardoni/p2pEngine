9982d357dcfcada92f8ddeda1a060e9e7270b445
Prudhomme Julien <julien.prudhomme@etu.univ-amu.fr>
2015-07-15 15:02:47 +0200

    fav

--- src/main/java/model/data/favorites/Favorites.java
+++ src/main/java/model/data/favorites/Favorites.java
@@ -172,7 +172,7 @@ public class Favorites extends AbstractAdvertisement{
 	///////////////////////////////////////////////// ADVERTISEMENT \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 	@Override
 	protected String getAdvertisementName() {
-		return Favorites.class.getName();
+		return getClass().getName();
 	}
 	@Override
 	protected void setKeys() {
